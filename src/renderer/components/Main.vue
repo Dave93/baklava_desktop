83d844fdecff01e5cde8bda7f5ca566fb1f1eb8a
Dave93 <yul.davron.93@gmail.com>
2020-09-24 00:35:27 +0500

    sssaa

--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -490,7 +490,6 @@
                             @click="printOrder"
                             v-bind="attrs"
                             v-on="on"
-                            disabled
                           >
                             <v-icon large>mdi-printer</v-icon>
                           </v-btn>
@@ -956,6 +955,9 @@ import vSelect from "vue-select";
 import CartItemDelete from "./CartItemDelete";
 import MoneyColumn from "./MoneyColumn";
 const { ipcRenderer } = require("electron");
+const escpos = require("escpos");
+// install escpos-usb adapter module manually
+escpos.USB = require("escpos-usb");
 
 ModuleRegistry.registerModules([ClientSideRowModelModule, MasterDetailModule]);
 
@@ -1259,6 +1261,42 @@ export default {
       const managerLastName = this.managerData.LAST_NAME;
       const { orderId, storeData } = this.orderData;
       const address = storeData ? storeData.ADDRESS : "";
+
+      // Select the adapter based on your printer type
+      const device = new escpos.USB();
+      // const device  = new escpos.Network('localhost');
+      // const device  = new escpos.Serial('/dev/usb/lp0');
+
+      const options = { encoding: "GB18030" /* default */ };
+      // encoding is optional
+
+      const printer = new escpos.Printer(device, options);
+
+      device.open(function (error) {
+        // printer
+        //   .font("a")
+        //   .align("ct")
+        //   .style("bu")
+        //   .size(1, 1)
+        //   .text("The quick brown fox jumps over the lazy dog")
+        //   .text("敏捷的棕色狐狸跳过懒狗")
+        //   .barcode("1234567", "EAN8")
+        //   .table(["One", "Two", "Three"])
+        //   .tableCustom(
+        //     [
+        //       { text: "Left", align: "LEFT", width: 0.33, style: "B" },
+        //       { text: "Center", align: "CENTER", width: 0.33 },
+        //       { text: "Right", align: "RIGHT", width: 0.33 },
+        //     ],
+        //     { encoding: "cp857", size: [1, 1] } // Optional
+        //   )
+        //   .qrimage("https://github.com/song940/node-escpos", function (err) {
+        //     this.cut();
+        //     this.close();
+        //   });
+
+        printer.font("a").align("ct").style("b").text("g").cut().close();
+      });
 
       let discountPrintValue = "";
       if (discountValue) {
