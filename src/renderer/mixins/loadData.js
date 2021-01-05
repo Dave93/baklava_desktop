0717856a943b674df0f926d8da7ee8dd0970b841
Dave93 <yul.davron.93@gmail.com>
2021-01-05 11:12:45 +0500

    hide missing prods

--- src/renderer/mixins/loadData.js
+++ src/renderer/mixins/loadData.js
@@ -26,20 +26,25 @@ export default {
             let {data: productsData} = await this.$http.get(
                 `${webHook}mycatalog.product.list?managerId=${managerData.ID}`
             );
+
+            let resultData = productsData.result.map((item) => ({
+                id: item.ID,
+                name: item.ELEMENT_NAME,
+                barcode: item.BARCODE_BARCODE,
+                customCode: item.CUSTOM_CODE,
+                selected: false,
+                categoryId: item.ELEMENT_IBLOCK_SECTION_ID,
+                image: item.PREVIEW_PICTURE,
+                price: item.BASE_PRICE,
+                type: item.type,
+                totalAmountCount: item.TOTAL_AMOUNT_COUNT,
+                childs: item.childs,
+            }));
+
+            resultData = resultData.filter(item => item.totalAmountCount > 0);
+
             await ProductItems.insert({
-                data: productsData.result.map((item) => ({
-                    id: item.ID,
-                    name: item.ELEMENT_NAME,
-                    barcode: item.BARCODE_BARCODE,
-                    customCode: item.CUSTOM_CODE,
-                    selected: false,
-                    categoryId: item.ELEMENT_IBLOCK_SECTION_ID,
-                    image: item.PREVIEW_PICTURE,
-                    price: item.BASE_PRICE,
-                    type: item.type,
-                    totalAmountCount: item.TOTAL_AMOUNT_COUNT,
-                    childs: item.childs,
-                }))
+                data: resultData
             });
             await this.setCartItems({
                 val: []
